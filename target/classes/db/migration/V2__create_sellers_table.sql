bf5e4d885103389c212e47b11e722b5a141a14e1
Amin Albooyeh <amin.albooyeh@gmail.com>
2026-09-14 10:37:54 +0200

    Remove target/ from version control - add .gitignore

--- target/classes/db/migration/V2__create_sellers_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE sellers (
-    id         BIGSERIAL PRIMARY KEY,
-    name       VARCHAR(100) NOT NULL,
-    url        VARCHAR(500),
-    is_digikala BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP NOT NULL DEFAULT now()
-);
-
-CREATE INDEX idx_sellers_name ON sellers (name);
